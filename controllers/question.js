f6be2f78929005c469fabba8a5c7e0cffb9f79ee
Yumit Gyuler <yumit.gyuler@gmail.com>
2021-08-14 18:46:33 +0200

    Added Ask A New Question Function

--- controllers/question.js
+++ controllers/question.js
@@ -1,9 +1,24 @@
+const Question = require("../models/Question");
+const CustomError = require("../helpers/error/CustomError");
+const asyncErrorWrapper = require("express-async-handler");
+
 const getAllQuestions = (req, res, next) => {
   res.status(200).json({
     success: true,
   });
 };
 
-module.exports = {
-  getAllQuestions,
-};
+const askNewQuestions = asyncErrorWrapper(async (req, res, next) => {
+  const information = req.body;
+
+  const question = await Question.create({
+    ...information,
+    user: req.user.id,
+  });
+  res.status(200).json({
+    success: true,
+    data: question,
+  });
+});
+
+module.exports = { getAllQuestions, askNewQuestions };
--- routers/question.js
+++ routers/question.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getAllQuestions } = require("../controllers/question");
+const { getAllQuestions, askNewQuestions } = require("../controllers/question");
+const { getAccessToRoute } = require("../middlewares/authorization/auth");
 
 router.get("/", getAllQuestions);
+router.get("/ask", getAccessToRoute, askNewQuestions);
 
 module.exports = router;
